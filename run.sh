a7c9e214c34364ed580e26a236d90e46e2bce20c
aidiary <f2forest@gmail.com>
2017-04-05 22:09:19 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-for max_train in 50 100 1000 5000 20000; do
-    python sklearn_logreg.py $max_train
-done
+# for max_train in 50 100 1000 5000 20000; do
+#     python sklearn_logreg.py $max_train
+# done
+
+python train_notmnist.py --model LogReg
+python train_notmnist.py --model MLP --unit 1024
